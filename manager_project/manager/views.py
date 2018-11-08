8fd6d11016db64dee5994166dffa0082d747e19d
technicakidz <y.technicakidz08@gmail.com>
2018-11-08 18:59:53 +0900

    add:templates

--- manager_project/manager/views.py
+++ manager_project/manager/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'manager/post_list.html', {})
