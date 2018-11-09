9ffed9bebf53a8f1c20775ec18ed3289783147fd
technicakidz <y.technicakidz08@gmail.com>
2018-11-09 17:58:06 +0900

    tutorial03/#a-shortcut-render

--- manager_project/manager_project/urls.py
+++ manager_project/manager_project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('writers.urls')),
     path('writers/', include('writers.urls')),
     path('admin/', admin.site.urls),
 ]
--- manager_project/writers/views.py
+++ manager_project/writers/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
 
 from .models import Post
 
-def index(request):
+def post_list(request):
     post_list = Post.objects.order_by('-published_date')[:5]
     context = {'post_list': post_list}
-    return render(request, 'writers/index.html', context)
+    return render(request, 'writers/post_list.html', context)
 
 def detail(request, author_id):
     return HttpResponse("You're looking at author %s." % author_id)
