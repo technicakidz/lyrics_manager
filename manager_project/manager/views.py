859318376202345076698a1c74b50ffa02d37f8a
technicakidz <y.technicakidz08@gmail.com>
2018-11-09 14:39:04 +0900

    create:app dir

--- manager_project/manager/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Post
-
-def post_list(request):
-    Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'manager/post_list.html', {})
