f28667faee67e061699c4a6040a4b5f6eece96fb
technicakidz <y.technicakidz08@gmail.com>
2018-11-13 15:30:10 +0900

    タイトル一覧表示

--- manager_project/writers/views.py
+++ manager_project/writers/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render
 from .models import Post
 
 def post_list(request):
-    post_list = Post.objects.order_by('-published_date')[:5]
-    context = {'post_list': post_list}
+    post_lists = Post.objects.order_by('-published_date')[:5]
+    context = {'post_lists': post_lists}
     return render(request, 'writers/post_list.html', context)
 
 def detail(request, author_id):
