efbd23a5d70aafd437e7d20a84056697647a60e3
technicakidz <perfumemarch08@yahoo.co.jp>
2018-11-19 23:13:51 +0900

    fix:routing

--- manager_vue/babel.config.js
+++ manager_vue/babel.config.js
@@ -1,5 +1,25 @@
 module.exports = {
   presets: [
     '@vue/app'
-  ]
+  ],
+  dev: {
+    proxyTable: {
+      '/api' : {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': 'api'
+        }
+      }
+    }
+  },
+  cssSourceMap: false,
+  build: {
+    index: path.resolve(__dirname, '../templates/index.html'),
+
+    // Paths
+    assetsRoot: path.resolve(__dirname, '../static'),
+    assetsSubDirectory: 'build',
+    assetsPublicPath: '/static/',
+  }
 }
